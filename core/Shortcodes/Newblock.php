b1410286365619383f4f684c9c35fb90adad5d78
Matt Knighton <matt.knighton@mrkdevelopment.com>
2023-07-05 12:44:07 +1000

    Update Newblock.php

--- core/Shortcodes/Newblock.php
+++ core/Shortcodes/Newblock.php
@@ -30,7 +30,7 @@ class Newblock extends BaseController {
 	 */
 	public function mrk_newblock_shortcode() {
 
-		wp_enqueue_style( 'newblock-shortcode-style', $this->plugin_url . 'assets/css/newblock.css' );
+		wp_enqueue_style( 'newblock-shortcode-style', $this->plugin_url . 'assets/newblock.css' );
 
 		$html = '<h2>New block Shortcode</h2>';
 
